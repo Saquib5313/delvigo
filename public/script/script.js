7d86814ac0aabbc80c2041165d141859b518f588
Saquib <mohammadsaquib072@gmail.com>
2026-04-21 15:13:22 +0530

    abc

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,15 @@
+AOS.init();
+
+const c1 = new CountUp('count1', 500);
+const c2 = new CountUp('count2', 120);
+const c3 = new CountUp('count3', 25);
+
+c1.start();
+c2.start();
+c3.start();
+
+
+
 
 window.addEventListener("load", () => {
   const video = document.getElementById("introVideo");
