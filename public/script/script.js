fd58c044c2afd024c64eb3fbd114f03d01157e37
Saquib <mohammadsaquib072@gmail.com>
2026-04-28 13:30:53 +0530

    abc

--- public/script/script.js
+++ public/script/script.js
@@ -6,14 +6,21 @@ window.addEventListener("load", () => {
   const video = document.getElementById("introVideo");
   const loader = document.getElementById("video-loader");
   const content = document.getElementById("main-content");
-
+  
   // ensure video starts
+  if(!sessionStorage.getItem('played')){
+
+    loader.style.display='flex'
+    content.style.display = "none";
+  
   video.play().catch(() => {});
 
   // when video ends
   video.onended = () => {
     loader.style.transition = "opacity 0.8s ease";
     loader.style.opacity = "0";
+
+    sessionStorage.setItem('played','true')
 
     setTimeout(() => {
       loader.style.display = "none";
@@ -32,6 +39,15 @@ window.addEventListener("load", () => {
     easing: 'ease-in-out'
   });
   }, 5000); // if video fails
+}
+else{
+  AOS.init({
+    duration: 1000,     // animation duration
+    once: true,         // animation happens only once
+    offset: 100,        // trigger offset
+    easing: 'ease-in-out'
+  });
+}
 });
 
 
